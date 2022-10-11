57038c864e431491a40c30b61c4072c678f0b251
Tomas <tomibudeguer20@gmail.com>
2022-10-11 14:53:26 -0300

    hice la funcion onsubmit el formulario con las validadiones

--- src/components/FormClima.jsx
+++ src/components/FormClima.jsx
@@ -1,20 +1,45 @@
+import { useState } from "react";
 import { Card } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import CardClima from "./CardClima";
 
 const FormClima = () => {
+  const [ciudad, setCiudad] = useState("");
+  const [pais, setPais] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if(ciudad.trim() === '' || pais.trim() === ''){
+        alert("Complete los dos campos por favor");
+    }else {
+        setCiudad('')
+        setPais('')
+      }
+  }
   return (
     <Card>
       <Card.Header>
-        <Form>
+        <Form onSubmit={handleSubmit}>
           <Form.Group className="mb-3" controlId="formBasicCiudad">
             <Form.Label>Ciudad</Form.Label>
-            <Form.Control type="text" placeholder="Ej: San Miguel de Tucuman" />
+            <Form.Control
+              type="text"
+              placeholder="Ej: San Miguel de Tucuman"
+              onChange={(e) => setCiudad(e.target.value)}
+              value={ciudad}
+              required
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPais">
             <Form.Label>Pais</Form.Label>
-            <Form.Control type="text" placeholder="Ej: Argentna" />
+            <Form.Control
+              type="text"
+              placeholder="Ej: Argentna"
+              onChange={(e) => setPais(e.target.value)}
+              value={pais}
+              required
+            />
           </Form.Group>
           <Button variant="primary" type="submit">
             Enviar
